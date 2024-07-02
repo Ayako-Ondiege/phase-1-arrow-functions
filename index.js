29aaf21fb7fb19549e863c058af540afb22a5936
jagedos <jagedosusrecomm@gmail.com>
2024-07-02 22:39:17 +0300

    arrow functions codes

---
+++ index.js
@@ -0,0 +1,32 @@
+// function foo(){
+//     return 'bar';
+// }
+
+// const foo = function (){
+//     return bar
+// }
+
+// function divide(x,y){
+    
+//     return (2000/100);
+// }
+
+// console.log(divide)
+
+const divide = function (x,y){
+   return x/y;
+}
+
+console.log(divide(2000,100));
+
+
+const square = (par) => {return par*par;}; 
+
+const add = (par1,par2) => {return par1+par2;};
+
+// (2000,100)=> 2000/100;
+
+
+
+// square.map()
+
